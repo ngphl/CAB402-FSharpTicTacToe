8e59e560951cb6d1b5372fa947fca2020ea14ac8
Rick Mccasker <rickmccasker@gmail.com>
2019-04-11 12:25:25 +1000

    Impure pass game start

--- FSharpTicTacToeModels/TicTacToeImpure.fs
+++ FSharpTicTacToeModels/TicTacToeImpure.fs
@@ -2,32 +2,44 @@ namespace QUT
 
     module FSharpImpureTicTacToeModel =
     
-        type Player = Something (* implement type *)
+        type Player = Nought | Cross
 
         type GameState = 
-            { something: int (* implement type *) } 
+            { mutable currentTurn: Player; boardSize: int; board: string [,] } 
             interface ITicTacToeGame<Player> with
-                member this.Turn with get()    = raise (System.NotImplementedException("getTurn"))
-                member this.Size with get()    = raise (System.NotImplementedException("getSize"))
-                member this.getPiece(row, col) = raise (System.NotImplementedException("getPiece"))
+                member this.Turn with get()    = this.currentTurn
+                member this.Size with get()    = this.boardSize
+                member this.getPiece(row, col) = this.board.[row, col]
 
         type Move = 
-            { something: int (* implement type *) }
+            { row: int; col: int }
             interface ITicTacToeMove with
-                member this.Row with get() = raise (System.NotImplementedException("getRow"))
-                member this.Col with get() = raise (System.NotImplementedException("getCol"))
+                member this.Row with get() = this.row
+                member this.Col with get() = this.col
 
 
 
-        let GameOutcome game     = raise (System.NotImplementedException("GameOutcome"))
+        let GameOutcome game = 
+            Draw
 
-        let ApplyMove game move  = raise (System.NotImplementedException("ApplyMove"))
+        let ApplyMove (game:GameState) move = 
+            let token = if game.currentTurn = Nought then "O" else "X"
+            game.board.[move.row, move.col] <- token
+            game.currentTurn <- if game.currentTurn = Nought then Cross else Nought
+            game
 
-        let CreateMove row col   = raise (System.NotImplementedException("CreateMove"))
+        let CreateMove row col = 
+            let move = { row = row; col = col }
+            move
 
-        let FindBestMove game    = raise (System.NotImplementedException("FindBestMove"))
+        let FindBestMove game =
+            //
+            raise (System.NotImplementedException("FindBestMove"))
 
-        let GameStart first size = raise (System.NotImplementedException("GameStart"))
+        let GameStart first size = 
+            let createBoard = array2D [| for x in 1..size -> [|for y in 1..size -> ""|]|]
+            let gamestate = {currentTurn = first; boardSize = size; board = createBoard}
+            gamestate
 
         // plus other helper functions ...
 
@@ -37,8 +49,8 @@ namespace QUT
         type WithAlphaBetaPruning() =
             override this.ToString()         = "Impure F# with Alpha Beta Pruning";
             interface ITicTacToeModel<GameState, Move, Player> with
-                member this.Cross with get()             = raise (System.NotImplementedException("getCross"))
-                member this.Nought with get()            = raise (System.NotImplementedException("getNought"))
+                member this.Cross with get()             = Cross
+                member this.Nought with get()            = Nought
                 member this.GameStart(firstPlayer, size) = GameStart firstPlayer size
                 member this.CreateMove(row, col)         = CreateMove row col
                 member this.GameOutcome(game)            = GameOutcome game 
