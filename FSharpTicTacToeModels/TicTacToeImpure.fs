b778e85bea2214dbe229d326a40af74e40aa6b3d
Rick Mccasker <rickmccasker@gmail.com>
2019-04-11 15:57:29 +1000

    Immutable and dirty implementation of gameoutcome

--- FSharpTicTacToeModels/TicTacToeImpure.fs
+++ FSharpTicTacToeModels/TicTacToeImpure.fs
@@ -17,10 +17,39 @@ namespace QUT
                 member this.Row with get() = this.row
                 member this.Col with get() = this.col
 
-
-
         let GameOutcome game = 
-            Draw
+            let mutable result = Draw
+            let size = game.boardSize
+            let getRow i (arr:string [,]) = arr.[i..i, *] |> Seq.cast<string>
+            let getCol i (arr:string [,]) = arr.[*, i..i] |> Seq.cast<string>
+            let getLDiag (arr:string [,]) = seq { for i in 0..size-1 do yield arr.[i,i]}
+            let getRDiag (arr:string [,]) = seq { for i in 0..size-1 do yield arr.[size-1-i,i]}
+
+            let checkLine line coordGenerator = 
+                if result = Undecided || result = Draw then
+                    if line |> Seq.contains ""
+                    then 
+                        result <- Undecided
+                    elif Seq.forall (fun x -> x = "X") line then result <- Win(Cross, coordGenerator)
+                    elif Seq.forall (fun x -> x = "O") line then result <- Win(Nought, coordGenerator)
+                    
+            let board = game.board
+            for i in 0..size-1 do
+                let row = getRow i board
+                let col = getCol i board
+                let ldiag = getLDiag board
+                let rdiag = getRDiag board
+                let genRowLine = seq { for y in 0..size-1 -> (i,y)}
+                let genColLine = seq { for x in 0..size-1 -> (x, i)}
+                let genLDiagLine = seq { for i in 0..size-1 -> (i,i)}
+                let genRDiagLine = seq { for i in 0..size-1 -> (size-1-i,i)}
+
+                checkLine row genRowLine
+                checkLine col genColLine
+                checkLine rdiag genRDiagLine
+                checkLine ldiag genLDiagLine
+
+            result
 
         let ApplyMove (game:GameState) move = 
             let token = if game.currentTurn = Nought then "O" else "X"
@@ -42,9 +71,7 @@ namespace QUT
             gamestate
 
         // plus other helper functions ...
-
-
-
+           
 
         type WithAlphaBetaPruning() =
             override this.ToString()         = "Impure F# with Alpha Beta Pruning";
